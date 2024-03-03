4ae3cfd5873081076af2f33a0b1b49dab81b891a
dariusplayz1 <97094335+dariusplayz1@users.noreply.github.com>
2024-03-03 14:54:47 -0800

    Update library.lua

--- library.lua
+++ library.lua
@@ -74,7 +74,7 @@ local library = {
         ['colortrans'] = 'https://raw.githubusercontent.com/portallol/luna/main/modules/trans.png';
     };
     numberStrings = {['Zero'] = 0, ['One'] = 1, ['Two'] = 2, ['Three'] = 3, ['Four'] = 4, ['Five'] = 5, ['Six'] = 6, ['Seven'] = 7, ['Eight'] = 8, ['Nine'] = 9};
-    signal = loadstring(game:HttpGet('https://raw.githubusercontent.com/dariusplayz1/testing/main/seel.lua'))();
+    signal = loadstring(game:HttpGet('https://raw.githubusercontent.com/dariusplayz1/dopamine/main/signal.lua'))();
     open = false;
     opening = false;
     hasInit = false;
